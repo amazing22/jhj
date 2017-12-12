989932377546149f983dbc2f65aa3f6b31b3d2a1
Your Name <you@example.com>
2017-12-12 20:06:14 +0900

    dfd

--- photo/forms.py
+++ photo/forms.py
@@ -2,6 +2,5 @@ from photo.models import Album, Photo
 from django.forms.models import inlineformset_factory
 
 PhotoInlineFormSet = inlineformset_factory(Album, Photo,
-    fields = ['image', 'title', 'description'],
+    fields = ['image','image', 'title', 'description'],
     extra = 2)
-
